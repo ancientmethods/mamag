3d425919c1644aee853c7b47e494263be4639720
Samer Haidar <samer.haidar@commusoft.co.uk>
2017-11-13 17:53:15 +0000

    fixed meal type fields

--- app/src/main/java/com/mamags/mamag/model/MealType.java
+++ app/src/main/java/com/mamags/mamag/model/MealType.java
@@ -1,12 +1,16 @@
 package com.mamags.mamag.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by samer on 11/11/2017.
  */
 
 public class MealType {
 
+    @SerializedName("Id")
     private int id;
+    @SerializedName("Description")
     private String description;
 
     public int getId() {
